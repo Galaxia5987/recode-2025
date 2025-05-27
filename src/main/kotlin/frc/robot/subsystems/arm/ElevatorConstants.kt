ce94b4a2075dfdfe22af0920878ebb2f596d5fcd
יהלי <mor.yahali1612@gmail.com>
2025-05-27 19:46:44 +0300

    Add constants

---
+++ src/main/kotlin/frc/robot/subsystems/arm/ElevatorConstants.kt
@@ -0,0 +1,9 @@
+package frc.robot.subsystems.arm
+
+import edu.wpi.first.units.Units
+import edu.wpi.first.units.measure.Distance
+
+val l4Height: Distance = Units.Meters.zero()
+val l3Height: Distance = Units.Meters.zero()
+val l2Height: Distance = Units.Meters.zero()
+val l1Height: Distance = Units.Meters.zero()
