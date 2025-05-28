2a0ad7e1b98e00d74745575802e2e6bf84ad07ee
rakrakon <68773850+rakrakon@users.noreply.github.com>
2025-05-28 14:02:50 +0300

    Add ClimberIO

---
+++ src/main/kotlin/frc/robot/subsystems/climb/ClimberIO.kt
@@ -0,0 +1,18 @@
+package frc.robot.subsystems.climb
+
+import edu.wpi.first.units.Units
+import edu.wpi.first.units.measure.Angle
+import edu.wpi.first.units.measure.Voltage
+
+interface ClimberIO {
+    val inputs: LoggedClimberInputs
+
+    fun setVoltage(voltage: Voltage) {}
+    fun updateInputs() {}
+
+    @org.team9432.annotation.Logged
+    open class ClimberInputs {
+        var applirdVoltage: Voltage = Units.Volts.zero()
+        var angle: Angle = Units.Degree.zero()
+    }
+}
