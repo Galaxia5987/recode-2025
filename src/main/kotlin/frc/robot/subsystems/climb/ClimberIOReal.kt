ccd6665022f0b339d7c63a03b40d855eeb0f827a
noga <noga.veksler@gmail.com>
2025-06-02 23:06:40 +0300

    Add ClimberIOReal.kt

---
+++ src/main/kotlin/frc/robot/subsystems/climb/ClimberIOReal.kt
@@ -0,0 +1,25 @@
+package frc.robot.subsystems.climb
+
+import com.ctre.phoenix6.controls.VoltageOut
+import com.ctre.phoenix6.hardware.TalonFX
+import edu.wpi.first.units.measure.Voltage
+
+class ClimberIOReal : ClimberIO {
+    override val inputs: LoggedClimberInputs = LoggedClimberInputs()
+
+    private val motor = TalonFX(0)
+    private val voltageControl = VoltageOut(0.0)
+
+    init {
+        motor.configurator.apply(MOTOR_CONFIG.MotorOutput)
+    }
+
+    override fun setVoltage(voltage: Voltage) {
+        motor.setControl(voltageControl.withOutput(voltage))
+    }
+
+    override fun updateInputs() {
+        inputs.angle = motor.position.value
+        inputs.appliedVoltage = motor.motorVoltage.value
+    }
+}
