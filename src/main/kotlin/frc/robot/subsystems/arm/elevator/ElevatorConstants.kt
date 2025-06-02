30a53086777230c6b4571bb71a7f909f1b8141d4
יהלי <mor.yahali1612@gmail.com>
2025-06-02 22:08:09 +0300

    Change known variables from robot-2025

--- src/main/kotlin/frc/robot/subsystems/arm/elevator/ElevatorConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/arm/elevator/ElevatorConstants.kt
@@ -15,11 +15,11 @@ import edu.wpi.first.units.Units
 import edu.wpi.first.units.measure.Distance
 import edu.wpi.first.units.measure.MomentOfInertia
 
-val l4Height: Distance = Units.Meters.zero()
-val l3Height: Distance = Units.Meters.zero()
-val l2Height: Distance = Units.Meters.zero()
-val l1Height: Distance = Units.Meters.zero()
-val TOLERANCE: Distance = Units.Meters.of(0.2)
+val l4Height: Distance = Units.Meters.of(0.0)
+val l3Height: Distance = Units.Meters.of(0.0)
+val l2Height: Distance = Units.Meters.of(15.0)
+val l1Height: Distance = Units.Meters.of(105.0)
+val TOLERANCE: Distance = Units.Centimeter.of(2.0)
 val RADIUS: Distance = Units.Centimeter.of(5.0)
 
 val jKgMetersSquared: MomentOfInertia = Units.KilogramSquareMeters.zero()
