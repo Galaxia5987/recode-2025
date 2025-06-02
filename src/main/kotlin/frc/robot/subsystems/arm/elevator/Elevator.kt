0fba1a092729f52aa4b2462a68d258c32fe2f4b0
יהלי <mor.yahali1612@gmail.com>
2025-06-02 21:37:25 +0300

    Change setPoint to be private

--- src/main/kotlin/frc/robot/subsystems/arm/elevator/Elevator.kt
+++ src/main/kotlin/frc/robot/subsystems/arm/elevator/Elevator.kt
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase
 import edu.wpi.first.wpilibj2.command.button.Trigger
 
 class Elevator(private val io: ElevatorIO) : SubsystemBase() {
-    var setPoint: Distance = Units.Meters.zero()
+    private var setPoint: Distance = Units.Meters.zero()
 
     val atSetpoint: Trigger = Trigger {
         io.getHeight().isNear(setPoint, TOLERANCE)
