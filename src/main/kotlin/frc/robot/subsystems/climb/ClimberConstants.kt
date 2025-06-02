64d26f799b6156b2793ec7d5d848e6b763229b93
noga <noga.veksler@gmail.com>
2025-06-02 23:06:22 +0300

    Add ClimberConstants.kt

---
+++ src/main/kotlin/frc/robot/subsystems/climb/ClimberConstants.kt
@@ -0,0 +1,19 @@
+package frc.robot.subsystems.climb
+
+import com.ctre.phoenix6.configs.TalonFXSConfiguration
+import com.ctre.phoenix6.signals.InvertedValue
+import com.ctre.phoenix6.signals.NeutralModeValue
+import frc.robot.lib.motors.TalonFXSim
+import frc.robot.lib.motors.TalonType
+
+var GRER_RATIO = 0.0
+var MOTOR_OF_INERTIA = 0.0
+var MOTOR_CONFIG =
+    TalonFXSConfiguration().apply {
+        MotorOutput.apply {
+            NeutralMode = NeutralModeValue.Brake
+            Inverted = InvertedValue.CounterClockwise_Positive
+        }
+    }
+var MOTOR_CONFIG_SIM =
+    TalonFXSim(1, GRER_RATIO, MOTOR_OF_INERTIA, 1.0, TalonType.KRAKEN_FOC)
