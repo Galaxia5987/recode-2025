2d85fcba54f495ecf4644b16b8eb93233f5e2aa2
יהלי <mor.yahali1612@gmail.com>
2025-06-02 21:31:54 +0300

    Add the constants from robot-2025

--- src/main/kotlin/frc/robot/subsystems/arm/elevator/ElevatorConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/arm/elevator/ElevatorConstants.kt
@@ -3,8 +3,8 @@ package frc.robot.subsystems.arm.elevator
 import edu.wpi.first.units.Units
 import edu.wpi.first.units.measure.Distance
 
-val l4Height: Distance = Units.Meters.zero()
-val l3Height: Distance = Units.Meters.zero()
-val l2Height: Distance = Units.Meters.zero()
-val l1Height: Distance = Units.Meters.zero()
-val TOLERANCE: Distance = Units.Meters.of(0.2)
+val l4Height: Distance = Units.Meters.of(0.0)
+val l3Height: Distance = Units.Meters.of(0.0)
+val l2Height: Distance = Units.Meters.of(15.0)
+val l1Height: Distance = Units.Meters.of(105.0)
+val TOLERANCE: Distance = Units.Centimeter.of(2.0)
