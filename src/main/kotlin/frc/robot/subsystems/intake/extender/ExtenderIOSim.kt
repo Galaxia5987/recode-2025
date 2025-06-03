e769250ea284a09dc04a2f14476583eaf81aacbf
יהלי <mor.yahali1612@gmail.com>
2025-06-03 19:48:14 +0300

    Create ExtenderIOSim

---
+++ src/main/kotlin/frc/robot/subsystems/intake/extender/ExtenderIOSim.kt
@@ -0,0 +1,43 @@
+package frc.robot.subsystems.intake.extender
+
+import com.ctre.phoenix6.controls.PositionVoltage
+import edu.wpi.first.units.Units
+import edu.wpi.first.units.measure.Distance
+import edu.wpi.first.wpilibj.Timer
+import frc.robot.lib.motors.TalonFXSim
+import frc.robot.lib.motors.TalonType
+import frc.robot.lib.toAngle
+import frc.robot.lib.toDistance
+
+class ExtenderIOSim : ExtenderIO {
+    override val inputs: LoggedExtenderInputs = LoggedExtenderInputs()
+    private val motor =
+        TalonFXSim(
+            MOTOR_ID,
+            GEAR_RATIO,
+            jKgMetersSquared.`in`(Units.KilogramSquareMeters),
+            CONVERSION_FACTOR,
+            TalonType.KRAKEN_FOC
+        )
+    private val voltagePositionRequest = PositionVoltage(0.0)
+
+    init {
+        motor.setController(PIDController)
+    }
+
+    override fun setLength(length: Distance) {
+        motor.setControl(
+            voltagePositionRequest.withPosition(
+                length.toAngle(RADIUS, GEAR_RATIO)
+            )
+        )
+    }
+
+    override fun updateInputs() {
+        inputs.length =
+            Units.Rotations.of(motor.position).toDistance(RADIUS, GEAR_RATIO)
+        inputs.current = motor.appliedCurrent
+        inputs.voltage = motor.appliedVoltage
+        motor.update(Timer.getFPGATimestamp())
+    }
+}
