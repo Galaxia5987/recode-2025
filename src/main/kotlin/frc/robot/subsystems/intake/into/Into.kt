9d850ddabe2b7e702164ada01b92f2817d2bf516
יהלי <mor.yahali1612@gmail.com>
2025-06-05 08:56:09 +0300

    Add Into

---
+++ src/main/kotlin/frc/robot/subsystems/intake/into/Into.kt
@@ -0,0 +1,29 @@
+package frc.robot.subsystems.intake.into
+
+import edu.wpi.first.units.Units
+import edu.wpi.first.units.measure.Voltage
+import edu.wpi.first.wpilibj2.command.SubsystemBase
+import edu.wpi.first.wpilibj2.command.button.Trigger
+import org.littletonrobotics.junction.AutoLogOutput
+import org.littletonrobotics.junction.Logger
+import org.team9432.annotation.Logged
+
+class Into(private val io: IntoIO) : SubsystemBase() {
+    var setVoltage = Units.Volt.zero()
+
+    @AutoLogOutput val onSetVoltage = Trigger { io.inputs.voltage == setVoltage}
+
+    fun setVoltage(voltage: Voltage) = run {
+        io.setVoltage(voltage)
+        setVoltage = voltage
+    }
+
+    fun inTake() = setVoltage(intakeVoltage)
+    fun outTake() = setVoltage(outtakeVoltage)
+
+    override fun periodic() {
+        io.updateInputs()
+        Logger.processInputs("Into",io.inputs)
+        Logger.recordOutput("IntoSetVoltage",setVoltage)
+    }
+}
