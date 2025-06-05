c8d2780cefb05e6a3ba5306796f5de94e09505fb
יהלי <mor.yahali1612@gmail.com>
2025-06-05 08:56:42 +0300

    Add IntoIOReal

---
+++ src/main/kotlin/frc/robot/subsystems/intake/into/IntoIOReal.kt
@@ -0,0 +1,21 @@
+package frc.robot.subsystems.intake.into
+
+import com.ctre.phoenix6.controls.PositionVoltage
+import com.ctre.phoenix6.controls.VoltageOut
+import com.ctre.phoenix6.hardware.TalonFX
+import edu.wpi.first.units.measure.Voltage
+
+class IntoIOReal() : IntoIO {
+    override val inputs: LoggedIntoInputs = LoggedIntoInputs()
+    private val motor = TalonFX(MOTOR_ID)
+    private val voltageRequest = VoltageOut(0.0)
+
+    override fun setVoltage(voltage: Voltage) {
+        motor.setControl(voltageRequest.withOutput(voltage))
+    }
+
+    override fun updateInputs(){
+        inputs.voltage = motor.supplyVoltage.value
+        inputs.current = motor.supplyCurrent.value
+    }
+}
