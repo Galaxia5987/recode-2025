016d8d9145ab36267fbff10ac3929fb56b907826
Noga18 <noga.veksler@gmail.com>
2025-06-23 13:12:58 +0300

    Update src/main/kotlin/frc/robot/subsystems/wrist/WristConstants.kt
    Change name
    
    Co-authored-by: Dan Katzuv <31829093+katzuv@users.noreply.github.com>

--- src/main/kotlin/frc/robot/subsystems/wrist/WristConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/wrist/WristConstants.kt
@@ -19,8 +19,8 @@ val FORWARD_SOFT_LIMIT = Rotation2d.fromRotations(1.8)
 val REVERSE_SOFT_LIMIT = Rotation2d.fromRotations(0.0)
 val GRER_RATIO = 1 / ((1.0 / 9.0) * (20.0 / 66.0) * (16.0 / 42.0))
 val MOMENT_OF_INERTIA: MomentOfInertia = Units.KilogramSquareMeters.of(0.001)
-var AT_SET_POINT: Angle = Units.Degrees.of(1.5)
-var NEAR_SET_POINT: Angle = Units.Degrees.of(10.0)
+var AT_SET_POINT_THERSHOLD: Angle = Units.Degrees.of(1.5)
+var NEAR_SET_POINT_THERSHOLD: Angle = Units.Degrees.of(10.0)
 val RESET_VOLTAGE: Voltage = Units.Volt.of(-10.0)
 var MOTOR_CONFIG =
     TalonFXSConfiguration().apply {
